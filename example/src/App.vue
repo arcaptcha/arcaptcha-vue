338aaac7e75c20735cd30583223a567ec4bdc6d8
haji4ref <haji4ref@gmail.com>
2021-12-29 21:55:51 +0330

    @ Adds example

---
+++ example/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <arcaptcha-vue
+            site_key="21smftb02t"
+            :callback="onSuccess"
+            ref="widget"
+        ></arcaptcha-vue>
+
+        <button @click="testReset">click</button>
+    </div>
+</template>
+
+<script>
+import ArcaptchaVue from "arcaptcha-vue";
+
+export default {
+    methods: {
+        onSuccess(token) {
+            console.log(token);
+        },
+        testReset() {
+            this.$refs.widget.resetCaptcha();
+        },
+    },
+    name: "App",
+    components: {
+        ArcaptchaVue,
+    },
+};
+</script>
