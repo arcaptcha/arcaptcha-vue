11f2639fa83bd51674508818cfc2e12929e994dd
hbn.yasaman <hbn.yasaman@gmail.com>
2022-09-01 15:31:52 +0430

    adds callbacks

--- src/arcaptcha-vue.vue
+++ src/arcaptcha-vue.vue
@@ -10,6 +10,18 @@ export default /*#__PURE__*/ {
     callback: {
       default: false,
     },
+    error_callback: {
+      default: false,
+    },
+    reset_callback: {
+      default: false,
+    },
+    expired_callback: {
+      default: false,
+    },
+    chlexpired_callback: {
+      default: false,
+    },
     color: {
       type: String,
       default: "normal",
@@ -61,10 +73,24 @@ export default /*#__PURE__*/ {
     reset() {
       arcaptcha.reset(this.widget_id);
     },
-    loadCaptcha() {
+    registerCallback() {
       if (this.callback)
         window[`arcaptcha_callback_${this.id}`] = this.callback;
-
+      if (this.error_callback)
+        window[`arcaptcha_error_callback_${this.id}`] =
+          this.error_callback;
+      if (this.reset_callback)
+        window[`arcaptcha_reset_callback_${this.id}`] =
+          this.reset_callback;
+      if (this.expired_callback)
+        window[`arcaptcha_expired_callback_${this.id}`] =
+          this.expired_callback;
+      if (this.chlexpired_callback)
+        window[`arcaptcha_chlexpired_callback_${this.id}`] =
+          this.chlexpired_callback;
+    },
+    loadCaptcha() {
+      this.registerCallback();
       this.widget_id = arcaptcha.render(`#${this.id}`, {
         "site-key": this.site_key,
         size: this.invisible ? "invisible" : "",
@@ -72,32 +98,44 @@ export default /*#__PURE__*/ {
         theme: this.theme,
         lang: this.lang,
         callback: this.callback ? `arcaptcha_callback_${this.id}` : null,
+        error_callback: this.error_callback
+          ? `arcaptcha_error_callback_${this.id}`
+          : null,
+        reset_callback: this.reset_callback
+          ? `arcaptcha_reset_callback_${this.id}`
+          : null,
+        expired_callback: this.expired_callback
+          ? `arcaptcha_expired_callback_${this.id}`
+          : null,
+        chlexpired_callback: this.chlexpired_callback
+          ? `arcaptcha_chlexpired_callback_${this.id}`
+          : null,
       });
     },
     loadScript(url) {
       //return new Promise(function (resolve) {
-        var my_script = document.head.querySelector("#arcptcha-script");
-        let script = my_script || document.createElement("script");
-        script.src = url;
-        script.id = "arcptcha-script";
-        if (!my_script) {
-          window.arcaptchaWidgetLoading = new Promise((resolve, reject) => {
-            script.onload = () => {
-              resolve();
-              this.initialize()
-            };
-          });
-        }
-        if (my_script) {
-          window.arcaptchaWidgetLoading.then(() => {
-            this.initialize()
-            //resolve();
-          });
-        }
-        if (!my_script) {
-          document.head.appendChild(script);
-        }
-   //   });
+      var my_script = document.head.querySelector("#arcptcha-script");
+      let script = my_script || document.createElement("script");
+      script.src = url;
+      script.id = "arcptcha-script";
+      if (!my_script) {
+        window.arcaptchaWidgetLoading = new Promise((resolve, reject) => {
+          script.onload = () => {
+            resolve();
+            this.initialize();
+          };
+        });
+      }
+      if (my_script) {
+        window.arcaptchaWidgetLoading.then(() => {
+          this.initialize();
+          //resolve();
+        });
+      }
+      if (!my_script) {
+        document.head.appendChild(script);
+      }
+      //   });
     },
     initialize() {
       this.loadCaptcha();
